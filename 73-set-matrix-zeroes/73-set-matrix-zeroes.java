dba4451e6a3dcc48603fd93e6874a8b979970b41
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-05-07 00:46:33 +0530

    Time: 1 ms (89.32%), Space: 54.2 MB (36.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,23 +1,36 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         List<Pair<Integer,Integer>> al=new ArrayList<>();
+        boolean firstRow=false,firstCol=false;
         for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[i].length;j++)
             {
                 if(matrix[i][j]==0)
-                    al.add(new Pair(i,j));
+                {   if(i==0) firstRow=true;
+                    if(j==0) firstCol=true;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
             }
         }
-        for(int i=0;i<al.size();i++)
+        for(int i=1;i<matrix.length;i++)
         {
-            int r=al.get(i).getKey();
-            int c=al.get(i).getValue();
-            for(int k=0;k<matrix[r].length;k++)
-                matrix[r][k]=0;
-            for(int k=0;k<matrix.length;k++)
-                matrix[k][c]=0;
+            for(int j=1;j<matrix[i].length;j++)
+            {
+                if(matrix[0][j]==0||matrix[i][0]==0)
+                {
+                   matrix[i][j]=0;
+                }
+            }
         }
-        
-    }
+        if(firstRow){
+            for(int i=0;i<matrix[0].length;i++)
+                matrix[0][i]=0;
+        }
+         if(firstCol){
+            for(int i=0;i<matrix.length;i++)
+                matrix[i][0]=0;
+        }
+    }    
 }
