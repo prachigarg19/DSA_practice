bb378820797f9e18f1bc89eb0c28860c20017c98
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-06-02 21:52:27 +0530

    Time: 12 ms (11.47%), Space: 44.1 MB (42.32%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -2,22 +2,28 @@ class Solution {
     int[][] dp;
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        dp = new int[triangle.size() + 1][triangle.get(triangle.size() - 1).size() + 1];
+        dp = new int[triangle.size() + 2][triangle.get(triangle.size() - 1).size() + 2];
 
         for (int i = 0; i < triangle.size(); i++) {
             for (int j = 0; j < triangle.get(triangle.size() - 1).size(); j++) 
                 dp[i][j] = (int) 1e6;
         }
-        return sum(0, 0, triangle);
-    }
-
-    public int sum(int i, int j, List<List<Integer>> triangle) {
-        if (j == triangle.size()) {
-            return 0;
+        
+        for (int j = triangle.size(); j >= 0; j--) {
+            for (int i = triangle.get(triangle.size() - 1).size(); i >= 0; i--) 
+            {
+                if (j == triangle.size()) {
+                dp[i][j]=0;
+                    continue;
+                }
+                
+                if (dp[i][j] != (int) 1e6) continue;
+                
+                dp[j][i] = Math.min(dp[j+1][i] , dp[j+1][i+1]) + triangle.get(j).get(i);
+            }
         }
-
-        if (dp[i][j] != (int) 1e6) return dp[i][j];
-
-        return dp[i][j] = Math.min(sum(i, j + 1, triangle), sum(i + 1, j + 1, triangle)) + triangle.get(j).get(i);
+        return dp[0][0];
     }
+
+    
 }
