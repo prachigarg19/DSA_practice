4b1c611e5326b93c27d84113af7b4ccf96530925
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-06-13 17:29:40 +0530

    Time: 10 ms (16.62%), Space: 43.6 MB (68.73%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,19 +1,22 @@
 class Solution {
     int[][] dp;
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        dp=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int i=0;i<dp.length;i++)
-            Arrays.fill(dp[i],(int)1e6);
-        return per(triangle,0,0);
-    }
-    public int per(List<List<Integer>> triangle, int r, int c){
-        
-        if(r==triangle.size())
-            return 0;
-        if(c>triangle.get(r).size())
-            return (int)1e6;
-        if(dp[r][c]!=(int)1e6) return dp[r][c];
-        
-        return dp[r][c]=Math.min(per(triangle,r+1,c),per(triangle,r+1,c+1))+triangle.get(r).get(c);
+        dp = new int[triangle.size() + 3][triangle.get(triangle.size() - 1).size() + 3];
+        for (int i = 0; i < dp.length; i++) Arrays.fill(dp[i], (int) 1e6);
+
+        for (int r = triangle.size(); r >= 0; r--) {
+            for (int c = triangle.get(triangle.size() - 1).size() - 1; c >= 0; c--) {
+                if (r == triangle.size()) {
+                    dp[r][c] = 0;
+                    continue;
+                }
+
+                if (dp[r][c] != (int) 1e6) continue;
+                if (c >= triangle.get(r).size()) continue;
+                dp[r][c] = Math.min(dp[r + 1][c], dp[r + 1][c + 1]) + triangle.get(r).get(c);
+            }
+        }
+        return dp[0][0];
     }
 }
