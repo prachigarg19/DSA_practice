c90e2606ed38fb19a273b1bf5e43a174626df232
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-06-28 13:52:10 +0530

    Time: 224 ms (10.51%), Space: 117.3 MB (7.99%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,30 +1,21 @@
 class Solution {
-    int dp[][];
+    int[][] dp;
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int a : nums) {
-            sum += a;
-        }
+        for (int i : nums) sum += i;
         if (sum % 2 != 0) return false;
-
-        dp = new int[nums.length + 1][sum / 2 + 1];
-
-        for (int i = 0; i < dp.length; i++) Arrays.fill(dp[i], -1);
-        return per(nums, 0, sum / 2) == 1 ? true : false;
+        dp = new int[nums.length][sum + 1];
+        for (int[] i : dp) Arrays.fill(i, -1);
+        return find(nums, 0, sum / 2)==1?true:false;
     }
 
-    public int per(int[] nums, int i, int sum) {
-        if (sum == 0) return 1;
-        if (sum < 0) return 0;
-        if (i == nums.length) {
-            return 0;
-        }
+    public int find(int[] nums, int i, int s) {
+        if (s == 0) return 1;
+        if (i >= nums.length) return 0;
+        if (s < 0) return 0;
 
-        if (dp[i][sum] != -1) return dp[i][sum];
-
-        int inc = per(nums, i + 1, sum - nums[i]);
-        int exc = per(nums, i + 1, sum);
-        return dp[i][sum] = (inc | exc);
+        if (dp[i][s] != -1) return dp[i][s];
+        return dp[i][s] = find(nums, i + 1, s - nums[i]) | find(nums, i + 1, s);
     }
 }
