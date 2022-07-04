eab3ecd3c393b2142b5743c3f8b3ea3908c2ff7b
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-07-04 17:46:24 +0530

    Time: 1 ms (33.33%), Space: 41.9 MB (30.33%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,18 +1,20 @@
 class Solution {
+    int[] dp;
     public int wiggleMaxLength(int[] nums) {
-        int ans = 1;
-        
-        int pDiff = 0;
-        int i = 0;
-        while(i < nums.length-1){
-            int diff = nums[i+1] - nums[i];
-            if( (diff > 0 && pDiff <= 0) ||
-                 (diff < 0 && pDiff >=0) ) {
-                ans++;
-                pDiff = diff;
-            }
-            i++;
-        }
-        return ans;
+         dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return find(nums, 0, -1, -1001);
+    }
+
+    public int find(int[] nums, int ind, int previ, int prevs) {
+        if (ind >= nums.length) return 0;
+        if (prevs != -1001 && prevs * (nums[ind] - nums[previ]) > 0) return 0;
+        if( previ != -1 && nums[ind] == nums[previ]) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int inc = 0, exc = 0;
+        if (previ == -1) inc = find(nums, ind + 1, ind, prevs); 
+        else inc = find(nums, ind + 1, ind, nums[ind] - nums[previ]);
+        exc = find(nums, ind + 1, previ, prevs);
+        return dp[ind]=Math.max(inc+1, exc);
     }
 }
