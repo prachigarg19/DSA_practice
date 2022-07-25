cfd9db74b14698fc1407c099652ed4f0db7ef942
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-07-26 00:10:34 +0530

    Time: 21 ms (42.68%), Space: 50.2 MB (70.06%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -3,12 +3,14 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> hm=new HashMap<>();
         //storing only key while mantaning order using value
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->hm.get(a)-hm.get(b));
         for(int i:nums){
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
         for( Integer s: hm.keySet()){
             pq.add(s);
+            while(pq.size()>k)
+               pq.remove(); 
         }
         int[] ans=new int[k];
         while(k-->0){
