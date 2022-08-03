9322cac0b725f00cc21939afab89dad6a3467731
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-08-03 11:22:55 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (59.71%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     int[] dp;
     public int climbStairs(int n) {
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return per(n);
+        dp=new int[n];
+        return solve(n,0);
     }
-    public int per( int n){
-        if(n==1 || n==2) return n;
-        
-        if(n<=0) return 0;
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        return dp[n] = per(n-1)+per(n-2);
+    public int solve(int n, int i){
+        if(i==n)
+            return 1;
+        if(i>n)
+            return 0;
+        if(dp[i]!=0)
+            return dp[i];
+        return dp[i]=solve(n,i+1)+solve(n,i+2);
     }
 }
