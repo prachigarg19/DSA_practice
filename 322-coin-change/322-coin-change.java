248b3a0fe9b3aa9671818376eec76ab801f775df
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-08-04 18:32:58 +0530

    Time: 91 ms (13.06%), Space: 58.5 MB (11.44%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,20 @@
-public class Solution {
-public int coinChange(int[] coins, int amount) {
-    if(amount<1) return 0;
-    int[] dp = new int[amount+1];
-    int sum = 0;
-    
-	while(++sum<=amount) {
-		int min = -1;
-    	for(int coin : coins) {
-    		if(sum >= coin && dp[sum-coin]!=-1) {
-    			int temp = dp[sum-coin]+1;
-    			min = min<0 ? temp : (temp < min ? temp : min);
-    		}
-    	}
-    	dp[sum] = min;
-	}
-	return dp[amount];
-}
+class Solution {
+    int[][] dp;
+
+    public int coinChange(int[] coins, int amount) {
+        dp = new int[coins.length + 1][amount + 1];
+        for (int[] i : dp) Arrays.fill(i, -1);
+        int ans = solve(coins, 0, amount);
+        return ans==(int)1e6?-1:ans;
+    }
+
+    public int solve(int[] coins, int i, int s) {
+        if(s==0) return 0;
+        if(s<0) return (int)1e6;
+        if(i>=coins.length) return (int)1e6;
+        if(dp[i][s]!= -1)
+            return dp[i][s];
+        dp[i][s]=Math.min(solve(coins,i,s-coins[i])+1,solve(coins,i+1,s));
+        return dp[i][s];
+    }
 }
