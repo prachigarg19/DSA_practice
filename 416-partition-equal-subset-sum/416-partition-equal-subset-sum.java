d40b31e2eadf37ed187e5391858dc3f6b7e90165
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-08-07 13:56:36 +0530

    Time: 72 ms (47.96%), Space: 68 MB (24.25%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,26 +1,35 @@
 class Solution {
     int[][] dp;
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int i: nums){
+        int sum=0;
+        for(int i: nums)
             sum+=i;
-        }
         if(sum%2!=0) return false;
         dp=new int[nums.length+1][sum/2+1];
-        for(int[] o: dp)
+         for(int[] o: dp)
             Arrays.fill(o,-1);
-        return solve(nums, 0, sum/2) == 1? true: false;
-    }
-    public int solve( int[] nums, int i, int sum){
-        if(sum == 0)
-            return 1;
-        if(sum < 0)
-            return 0;
-        
-        if(i>=nums.length)
-            return 0;
-        if(dp[i][sum]!=-1)
-            return dp[i][sum];
-        return dp[i][sum]=solve(nums,i+1,sum-nums[i])|solve(nums,i+1,sum);
+        for(int i=0;i<dp.length;i++)
+            dp[i][0]=1;
+        for(int j=0;j<dp[0].length;j++)
+            dp[0][j]=0;
+        dp[0][0]=1;
+        for(int i=1;i<dp.length-1;i++){
+            for(int j=1;j<=sum/2;j++){
+               
+                if(dp[i][j]!=-1)
+                    continue;
+                if(j-nums[i]>=0)
+                dp[i][j] = dp[i-1][j-nums[i]]|dp[i-1][j];
+                else 
+                    dp[i][j] = dp[i-1][j];
+                
+            }
+         }
+        // for( int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++)
+        //         System.out.print(dp[i][j]+" ");
+        //     System.out.println();
+        // }
+        return dp[nums.length-1][sum/2]==1?true:false;
     }
 }
