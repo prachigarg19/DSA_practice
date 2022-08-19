0f9bd186d9bab03a9ae1d85f6c4fd6834edd9fd2
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2022-08-19 12:36:07 +0530

    Time: 94 ms (53.01%), Space: 87.5 MB (51.12%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        HashMap<Integer,Integer> hm= new HashMap<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        
         for(int i: arr){
-            hm.put(i,hm.getOrDefault(i,0)+1);
+            hm.put(i, hm.getOrDefault(i,0)+1);
         }
-        for(Integer i: hm.values()){
+        for(Integer i: hm.values())
             pq.add(i);
-        }
-        int sum=0,count=0;
+        
+        int count=0, sum=0;
         while(sum<arr.length/2){
-            if(pq.size()==0) break;
             sum+=pq.poll();
             count++;
         }
